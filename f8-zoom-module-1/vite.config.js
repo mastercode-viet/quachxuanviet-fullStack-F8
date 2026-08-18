e58adeb13eb7ef5882a1e0d729c5b5dfda05c6d8
mastercode-viet <quachviet010@gmail.com>
2026-08-18 11:55:06 +0700

    feat(module-1): Finished

---
+++ f8-zoom-module-1/vite.config.js
@@ -0,0 +1,28 @@
+import { defineConfig } from "vite";
+import { ViteEjsPlugin } from "vite-plugin-ejs";
+import path from "path";
+
+export default defineConfig({
+    base: process.env.NODE_ENV === "production" ? "/f8-zoom-module-1" : "/",
+    root: "src",
+    publicDir: "../public",
+     server: {
+        open: true,
+    },
+
+    build: {
+        outDir: "../dist",
+        emptyOutDir: true,
+        rollupOptions: {
+            input: {
+                index: path.resolve(__dirname, "src/index.html"),
+                about: path.resolve(__dirname, "src/about.html"),
+                blog: path.resolve(__dirname, "src/blog.html"),
+                contact: path.resolve(__dirname, "src/contact.html"),
+                practice: path.resolve(__dirname, "src/practice.html"),
+                
+            },
+        },
+    },
+    plugins: [ViteEjsPlugin()],
+});
